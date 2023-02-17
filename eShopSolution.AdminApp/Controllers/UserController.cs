7d33ab37358cebe2fa9dffc92dac43b6e6c2d012
MinhVV <minhvv@rikkeisoft.com>
2023-02-17 18:08:30 +0700

    phan trang cho pagenation

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -22,7 +22,7 @@ namespace eShopSolution.AdminApp.Controllers
             _configuration = configuration;
         }
 
-        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 10)
+        public async Task<IActionResult> Index(string keyword, int pageIndex = 1, int pageSize = 1)
         {
             var request = new GetUserPagingRequest()
             {
@@ -32,6 +32,13 @@ namespace eShopSolution.AdminApp.Controllers
             };
             var data = await _userApiClient.GetUserPaging(request);
             return View(data.ResultObj);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var result = await _userApiClient.GetByIdUser(id);
+            return View(result.ResultObj);
         }
 
         [HttpGet]
